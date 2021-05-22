fadaf695e49409d81cd14a3f0205986965ee4b06
Aarav <aarav.agarwal.000@gmail.com>
2021-05-22 17:36:39 -0400

    finish app 222

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -20,7 +20,7 @@ function Checkout() {
 		const stripe = await stripePromise;
 
 		const checkoutSession = await axios.post(
-			'/api/create-checkout-session',
+			"/api/create-checkout-session",
 			{
 				items: items,
 				email: session.user.email,
